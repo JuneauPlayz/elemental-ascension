34efd3c0995d6b1a26f25dcc89219c9cb310b8ab
JuneauYuki <cjquan02@gmail.com>
2025-03-21 00:45:10 -0700

    status text

--- scenes/reusables/status_info.gd
+++ scenes/reusables/status_info.gd
@@ -5,11 +5,24 @@ extends Control
 
 
 func update(status):
+	var run = get_tree().get_first_node_in_group("run")
 	match status.name:
 		"Burn":
 			status_name.text = "Burn (Does not stack)"
 			description.text = "This unit takes " + str(status.damage) + " " + status.element + " damage at the start of their turn"
 			turns_remaining.text = " Turns Remaining : " + str(status.turns_remaining)
+		"Nitro":
+			status_name.text = "Nitro (Does not stack)"
+			description.text = "This unit takes " + run.nitro_mult + "x damage, consumed when taking an instance of damage"
+		"Muck":
+			status_name.text = "Muck (Does not stacK)"
+			description.text = "This unit's next skill does 0.75x less damage"
+		"Sow":
+			status_name.text = "Sow (Can stack)"
+			description.text = "When a skill hits this unit, the caster of the skill heals " + run.sow_healing + " and gains " + run.sow_shielding + " shield"
+		"Bubble":
+			status_name.text = "Bubble (Does not stack)"
+			description.text = "This unit takes " + run.bubble_mult + "x damage"
 		_:
 			status_name.text = "Finish"
 			description.text = "the rest of the statuses u bob"
